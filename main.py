76ad4263f003202472ed063d7414b34ff213da28
every_one_callme_Pie <thananonza123@gmail.com>
2024-11-29 10:00:43 +0700

    i just apply transform function in the code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+
 import pandas as pd 
 import sqlalchemy
 import requests
+from google.colab import userdata
 
 
 class Config:
@@ -8,7 +10,7 @@ class Config:
   MYSQL_PORT = userdata.get("MYSQL_PORT") 
   MYSQL_USER = userdata.get("MYSQL_USER")
   MYSQL_PASSWORD = userdata.get("MYSQL_PASSWORD")
-  MYSQL_DB = 'YOUR_DB'
+  MYSQL_DB = 'r2de3'
   MYSQL_CHARSET = 'utf8mb4'
   
 engine = sqlalchemy.create_engine(
@@ -28,27 +30,34 @@ def convert_rate(price, rate):
   return price * rate
     
 # Pull API (Thai Currency) 
-url = "https://r2de3-currency-api-vmftiryt6q-as.a.run.app/gbp_thb"
-r = requests.get(url)
-result_conversion_rate = r.json()
-convert_rate =  pd.DataFrame(result_conversion_rate)
+def get_api_data(url):
+    r = requests.get(url)
+    r = r.json()
+    conversion_rate = pd.DataFrame(r)
+    conversion_rate = conversion_rate.drop(columns=['id'])
+    conversion_rate['date'] = pd.to_datetime(conversion_rate['date'])
+    return conversion_rate
 
-#FROM SQL TO DATAFRAME
-customer = pd.read_sql("SELECT * FROM r2de3.customer",engine)
-transaction = pd.read_sql("SELECT * FROM r2de3.transaction",engine)
-product = pd.read_sql("SELECT * FROM r2de3.product",engine)
+def get_data_from_db(db_name,customer_data,transaction_data,product_data,engine,API_data):
+    customer = pd.read_sql(f"SELECT * FROM {db_name}.{customer_data}",engine)
+    transaction = pd.read_sql(f"SELECT * FROM {db_name}.{transaction_data}",engine)
+    product = pd.read_sql(f"SELECT * FROM {db_name}.{product_data}",engine)
+    merged_transaction = transaction.merge(product, how="left", left_on="ProductNo", right_on="ProductNo").merge(customer, how="left", left_on="CustomerNo", right_on="CustomerNo")
+    final_df = merged_transaction.merge(API_data,how='left',left_on = 'Date' , right_on = 'date')
+    return final_df
 
-merged_transaction = transaction.merge(product, how="left", left_on="ProductNo", right_on="ProductNo").merge(customer, how="left", left_on="CustomerNo", right_on="CustomerNo")
 
-conversion_rate = conversion_rate.drop(columns=['id'])
-conversion_rate['date'] = pd.to_datetime(conversion_rate['date'])
+def Transform_data(final_df):
+    final_df["total_amount"] = final_df["Price"] * final_df["Quantity"]
+    final_df['thb_amount'] = final_df['total_amount'] * final_df['gbp_thb']
 
-final_df = merged_transaction.merge(conversion_rate,how='left',left_on = 'Date' , right_on = 'date')
-final_df["total_amount"] = final_df["Price"] * final_df["Quantity"]
-final_df['thb_amount'] = final_df['total_amount'] * final_df['gbp_thb']
+    final_df["thb_amount"] = final_df.apply(lambda row: convert_rate(row["total_amount"], row["gbp_thb"]), axis=1)
+    final_df = final_df.drop(["date", "gbp_thb"], axis=1)
+    final_df = final_df.drop(["date", "gbp_thb"], axis=1)
+    final_df.columns = ['transaction_id', 'date', 'product_id', 'price', 'quantity', 'customer_id',
+        'product_name', 'customer_country', 'customer_name', 'total_amount','thb_amount']
+    final_df.fillna({
+        'CustomerNo':'Unknown'
+    },inplace = True)
 
-final_df["thb_amount"] = final_df.apply(lambda row: convert_rate(row["total_amount"], row["gbp_thb"]), axis=1)
-final_df = final_df.drop(["date", "gbp_thb"], axis=1)
-final_df = final_df.drop(["date", "gbp_thb"], axis=1)
-final_df.columns = ['transaction_id', 'date', 'product_id', 'price', 'quantity', 'customer_id',
-       'product_name', 'customer_country', 'customer_name', 'total_amount','thb_amount']
+
